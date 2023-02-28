658cd911735d0bcb00b46e3107665c1b9d7f5246
Bavin Edwards <65621465+zerico007@users.noreply.github.com>
2023-02-28 12:00:11 -0500

    created PageHeader component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,14 @@
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "../styles/globals.scss";
+import PageHeader from "../components/PageHeader";
 
 const queryClient = new QueryClient();
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="logos">
-        <span
-          className="bitovi-logo"
-          role="presentation"
-          aria-label="bitovi-logo"
-        />
-        <img
-          src="https://www.freshbooks.com/wp-content/themes/freshpress/dist/images/logos/freshbooks-logo.svg"
-          alt="freshbooks logo"
-          width="155"
-          height="38"
-        />
-      </div>
+      <PageHeader />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
